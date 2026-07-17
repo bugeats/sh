ce69956cbb4eac1e1a5ea8b2df735570cf06bbff
Chadwick Dahlquist <chadwick@bugeats.com>
2026-07-17 16:09:34 -0700

    Update zellij theme colors

--- zellij/theme.nix
+++ zellij/theme.nix
@@ -10,114 +10,114 @@ in
       text_unselected {
         base ${rgb rgbcolors.COLOR_NORMAL_FG}
         background ${rgb rgbcolors.COLOR_NORMAL_BG}
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
+        emphasis_0 ${rgb rgbcolors.COLOR_ANSI_GREEN}
+        emphasis_1 ${rgb rgbcolors.COLOR_ANSI_CYAN}
+        emphasis_2 ${rgb rgbcolors.COLOR_ANSI_BLUE}
+        emphasis_3 ${rgb rgbcolors.COLOR_ANSI_MAGENTA}
       }
       text_selected {
-        base 251 241 199
-        background 80 73 69
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
+        base ${rgb rgbcolors.COLOR_NORMAL_FG}
+        background ${rgb rgbcolors.COLOR_SELECTION_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_ANSI_GREEN}
+        emphasis_1 ${rgb rgbcolors.COLOR_ANSI_CYAN}
+        emphasis_2 ${rgb rgbcolors.COLOR_ANSI_BLUE}
+        emphasis_3 ${rgb rgbcolors.COLOR_ANSI_MAGENTA}
       }
       ribbon_selected {
         base ${rgb rgbcolors.COLOR_UI_LEVEL_1_BG}
         background ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
-        emphasis_0 204 36 29
-        emphasis_1 214 93 14
-        emphasis_2 177 98 134
-        emphasis_3 69 133 136
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_1_BG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_1_BG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_1_BG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_1_BG}
       }
       ribbon_unselected {
         base ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
         background ${rgb rgbcolors.COLOR_UI_LEVEL_1_BG}
-        emphasis_0 204 36 29
-        emphasis_1 251 241 199
-        emphasis_2 69 133 136
-        emphasis_3 177 98 134
-      }
-      table_title {
-        base 152 151 26
-        background 0
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
-      }
-      table_cell_selected {
-        base 251 241 199
-        background 80 73 69
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
-      }
-      table_cell_unselected {
-        base 251 241 199
-        background 60 56 54
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
-      }
-      list_selected {
-        base 251 241 199
-        background 80 73 69
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
-      }
-      list_unselected {
-        base 251 241 199
-        background 60 56 54
-        emphasis_0 214 93 14
-        emphasis_1 104 157 106
-        emphasis_2 152 151 26
-        emphasis_3 177 98 134
-      }
-      frame_selected {
-        base ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
-        background ${rgb rgbcolors.COLOR_UI_LEVEL_2_BG}
-        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
-        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
-        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
-        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
-      }
-      frame_unselected {
-        base ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
-        background 0
-        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
         emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
-        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
         emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
       }
+      table_title {
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        background ${rgb rgbcolors.COLOR_UI_LEVEL_2_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+      }
+      table_cell_selected {
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        background ${rgb rgbcolors.COLOR_UI_LEVEL_3_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+      }
+      table_cell_unselected {
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        background ${rgb rgbcolors.COLOR_UI_LEVEL_2_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+      }
+      list_selected {
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        background ${rgb rgbcolors.COLOR_UI_LEVEL_3_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_3_FG}
+      }
+      list_unselected {
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        background ${rgb rgbcolors.COLOR_UI_LEVEL_2_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+      }
+      frame_selected {
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
+        background ${rgb rgbcolors.COLOR_NORMAL_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_1_FG}
+      }
+      frame_unselected {
+        base ${rgb rgbcolors.COLOR_NORMAL_BG}
+        background ${rgb rgbcolors.COLOR_NORMAL_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_NORMAL_BG}
+        emphasis_1 ${rgb rgbcolors.COLOR_NORMAL_BG}
+        emphasis_2 ${rgb rgbcolors.COLOR_NORMAL_BG}
+        emphasis_3 ${rgb rgbcolors.COLOR_NORMAL_BG}
+      }
       frame_highlight {
-        base 214 93 14
-        background 0
-        emphasis_0 177 98 134
-        emphasis_1 214 93 14
-        emphasis_2 214 93 14
-        emphasis_3 214 93 14
+        base ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        background ${rgb rgbcolors.COLOR_NORMAL_BG}
+        emphasis_0 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_1 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_2 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
+        emphasis_3 ${rgb rgbcolors.COLOR_UI_LEVEL_2_FG}
       }
       exit_code_success {
-        base 152 151 26
+        base ${rgb rgbcolors.COLOR_ANSI_GREEN}
         background 0
-        emphasis_0 104 157 106
-        emphasis_1 60 56 54
-        emphasis_2 177 98 134
-        emphasis_3 69 133 136
+        emphasis_0 ${rgb rgbcolors.COLOR_ANSI_CYAN}
+        emphasis_1 ${rgb rgbcolors.COLOR_ANSI_BLUE}
+        emphasis_2 ${rgb rgbcolors.COLOR_ANSI_MAGENTA}
+        emphasis_3 ${rgb rgbcolors.COLOR_ANSI_CYAN}
       }
       exit_code_error {
-        base 204 36 29
+        base ${rgb rgbcolors.COLOR_ANSI_RED}
         background 0
-        emphasis_0 215 153 33
-        emphasis_1 0
-        emphasis_2 0
-        emphasis_3 0
+        emphasis_0 ${rgb rgbcolors.COLOR_ANSI_YELLOW}
+        emphasis_1 ${rgb rgbcolors.COLOR_ANSI_RED}
+        emphasis_2 ${rgb rgbcolors.COLOR_ANSI_MAGENTA}
+        emphasis_3 ${rgb rgbcolors.COLOR_ANSI_YELLOW}
       }
       multiplayer_user_colors {
         player_1 177 98 134
